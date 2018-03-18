8dad598f81d4e08c9741214dfd3f6f6f6448a4f2
Zhou Peng <zhoupeng@tospur.com>
2018-03-18 15:56:20 +0800

    right package

--- src/main/scripts/start.sh
+++ src/main/scripts/start.sh
@@ -1,5 +1,3 @@
-@echo off
-@if not "%ECHO%" == ""  echo %ECHO%
-@if "%OS%" == "Windows_NT"  setlocal
+#!/usr/bin/bash
 
 java -jar -Dserver.conf="./conf/application.properties" -Dtemplate.dir="./template" -Dlog4j.configuration="file:./conf/log4j.properties" agent-control-server-1.0.jar
