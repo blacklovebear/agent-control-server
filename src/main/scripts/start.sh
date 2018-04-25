3abc549513bab114dcb2d3f7200d944b43302778
BlackZhou <zhoupengblack@icloud.com>
2018-04-25 10:43:57 +0800

    fix bug

--- src/main/scripts/start.sh
+++ src/main/scripts/start.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash
 
-java -jar -Dserver.conf="./conf/application.properties" -Dtemplate.dir="./template" -Dlog4j.configuration="file:./conf/log4j.properties" agent-control-server-1.0.jar
+ps -ef | grep agent-control-server | grep -v grep | awk '{print $2}' | xargs kill -9
+
+nohup java -jar -Dserver.conf="./conf/application.properties" -Dtemplate.dir="./template" \
+-Dlog4j.configuration="file:./conf/log4j.properties" agent-control-server-1.0.jar > logs/start.out 2>&1 &
