0bc203b092efe870ff40bcca05f168335316606a
ZhouPeng <you@example.com>
2018-05-14 15:44:39 +0800

    add stop file

--- bin/start.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-@if not "%ECHO%" == ""  echo %ECHO%
-@if "%OS%" == "Windows_NT"  setlocal
-
-java -jar -Dserver.conf="./conf/application.properties" -Dtemplate.dir="./template" -Dlog4j.configuration="file:./conf/log4j.properties" drc-streaming-control*.jar
