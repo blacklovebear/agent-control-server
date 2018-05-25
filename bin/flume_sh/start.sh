8ab554cfafa0b78a144a5d215363572d2b660404
ZhouPeng <you@example.com>
2018-05-25 15:06:54 +0800

    fix multi start bug

--- bin/flume_sh/start.sh
+++ bin/flume_sh/start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if ! mkdir /tmp/tagent.lock > /dev/null 2>&1
-then
-  echo "running, please stop first"
-else
-  nohup bin/flume-ng agent --conf conf --conf-file conf/canal-kafka.conf --name agent -Dflume.monitoring.type=http -Dflume.monitoring.port=34545 > logs/start.out 2>&1 &
+base=`dirname $0`
+if [ -f $base/bin/tagent.pid ] ; then
+  echo "found tagent.pid, please run stop.sh first, then start.sh" 2>&2
+  exit 0
 fi
+
+nohup bin/flume-ng agent --conf conf --conf-file conf/canal-kafka.conf --name agent -Dflume.monitoring.type=http -Dflume.monitoring.port=34545 > logs/start.out 2>&1 &
+echo $! > $base/bin/tagent.pid
--- bin/flume_sh/stop.sh
+++ bin/flume_sh/stop.sh
@@ -1,9 +1,65 @@
 #!/bin/bash
-var=`ps -ef | grep flume.node.Application | grep -v grep | awk '{print $2}'`
-if [ -z "$var" ]
-then
-  echo "tagent is not running"
-else
-  kill -15 $var
+
+cygwin=false;
+linux=false;
+case "`uname`" in
+    CYGWIN*)
+        cygwin=true
+        ;;
+    Linux*)
+    	linux=true
+    	;;
+esac
+
+get_pid() {	
+	STR=$1
+	PID=$2
+    if $cygwin; then
+        JAVA_CMD="$JAVA_HOME\bin\java"
+        JAVA_CMD=`cygpath --path --unix $JAVA_CMD`
+        JAVA_PID=`ps |grep $JAVA_CMD |awk '{print $1}'`
+    else
+    	if $linux; then
+	        if [ ! -z "$PID" ]; then
+	        	JAVA_PID=`ps -C java -f --width 1000|grep "$STR"|grep "$PID"|grep -v grep|awk '{print $2}'`
+		    else 
+		        JAVA_PID=`ps -C java -f --width 1000|grep "$STR"|grep -v grep|awk '{print $2}'`
+	        fi
+	    else
+	    	if [ ! -z "$PID" ]; then
+	        	JAVA_PID=`ps aux |grep "$STR"|grep "$PID"|grep -v grep|awk '{print $2}'`
+		    else 
+		        JAVA_PID=`ps aux |grep "$STR"|grep -v grep|awk '{print $2}'`
+	        fi
+	    fi
+    fi
+    echo $JAVA_PID;
+}
+
+base=`dirname $0`
+pidfile=$base/bin/tagent.pid
+if [ ! -f "$pidfile" ];then
+	echo "tagent is not running. exists"
+	exit
 fi
-rm -rf /tmp/tagent.lock
+
+pid=`cat $pidfile`
+if [ "$pid" == "" ] ; then
+	pid=`get_pid "flume.node.Application"`
+fi
+
+echo -e "`hostname`: stopping tagent $pid ... "
+kill $pid
+
+LOOPS=0
+while (true); 
+do 
+	gpid=`get_pid "flume.node.Application" "$pid"`
+    if [ "$gpid" == "" ] ; then
+    	echo "Oook! cost:$LOOPS"
+    	`rm $pidfile`
+    	break;
+    fi
+    let LOOPS=LOOPS+1
+    sleep 1
+done
